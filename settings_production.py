74a019ae6dd95c59cd6ebb7bb01d8849a4db6c5e
Anders Pearson <anders@columbia.edu>
2010-07-21 14:46:22 -0400

    supporting local settings override in production

--- settings_production.py
+++ settings_production.py
@@ -8,3 +8,10 @@ MEDIA_ROOT = '/var/www/maut/uploads/'
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from local_settings import *
+except ImportError:
+    pass
