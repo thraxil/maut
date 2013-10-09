bb933ddd00081c0b50ce217706aa7e421ae428e6
Anders Pearson <anders@columbia.edu>
2013-10-09 10:14:59 -0400

    corrected media path

--- maut/urls.py
+++ maut/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 import os.path
 admin.autodiscover()
 
-site_media_root = os.path.join(os.path.dirname(__file__), "media")
+site_media_root = os.path.join(os.path.dirname(__file__), "../media")
 
 urlpatterns = patterns(
     '',
