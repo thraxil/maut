997c731a7c9bdaf97740831956c2ee6c341b0c01
Anders Pearson <anders@columbia.edu>
2015-09-13 15:34:25 +0200

    convert to thraxilsettings

--- maut/settings_shared.py
+++ maut/settings_shared.py
@@ -1,126 +1,13 @@
 # Django settings for maut project.
 import os.path
-import sys
+from thraxilsettings.shared import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+app = 'maut'
+base = os.path.dirname(__file__)
 
-ADMINS = (
-    ('CCNMTL', 'anders@columbia.edu'),
-)
+locals().update(common(app=app, base=base))
 
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'maut',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-    }
-}
-if 'test' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '', }}
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-MEDIA_ROOT = "/var/www/maut/uploads/"
-MEDIA_URL = '/uploads/'
-ADMIN_MEDIA_PREFIX = '/media/'
-SECRET_KEY = ')ng#)ef_u@_^zvvu@dxm7ql-ybasdqweoiusdcv,.me-0bdm2q3oy8'
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.request',
-    'django.core.context_processors.static',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-)
-
-STATIC_URL = "/media/"
-STATIC_ROOT = ""
-STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(__file__), "../media"), )
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-ROOT_URLCONF = 'maut.urls'
-
-TEMPLATE_DIRS = (
-    "/var/www/maut/templates/",
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.admin',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+INSTALLED_APPS += [  # noqa
     'tagging',
     'maut.music',
-    'django_statsd',
-    'gunicorn',
-    'django_markwhat',
-    'compressor',
-    'smoketest',
 ]
-
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-
-THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[maut] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "maut@thraxil.org"
-
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'maut'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-
-HAKMES_BASE = "http://localhost:9300/"
-
-ALLOWED_HOSTS = ['localhost', '.thraxil.org']
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
